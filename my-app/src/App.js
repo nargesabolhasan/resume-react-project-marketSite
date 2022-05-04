21913b4b57ab35d5cc6e8a6ebe2db19859ef5cc5
narges Hajabolhasan <narges.abolhasan@gmail.com>
2022-05-04 15:07:55 +0430

    change nextjs to react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,19 @@
+import "./assets/Core-ui/Core-styles.scss"
+import axios from "axios"
+import HttpService from "./axios/HttpService"
+import react,{useState,useEffect} from "react"
 function App() {
+  
+
+  useEffect(() => {
+    // console.log(HttpService.get("orders",true))
+   let hpp=HttpService.get("products")
+  .then((response) => {console.log(response)})
+
+  })
+
   return (
-    <div>
+    <div style={{fontFamily:"koodak"}}>
      
     </div>
   );
