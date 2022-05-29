a6becf18ed369200163735fd21fe94209c888008
narges Hajabolhasan <narges.abolhasan@gmail.com>
2022-05-29 18:38:33 +0430

    feat : style of basket cells changed

--- src/components/user/TableBasket.js
+++ src/components/user/TableBasket.js
@@ -20,8 +20,6 @@ import Grid from "@mui/material/Grid";
 import Buttons from "../buttons/Button-add";
 import { setProducts, removeSelectedProduct } from "../../redux/basketSlice";
 
-
-
 const TableCells = styled("td")(({ theme }) => ({
   padding: theme.spacing(1),
   [theme.breakpoints.down("md")]: {
@@ -42,29 +40,29 @@ const TableCells = styled("td")(({ theme }) => ({
     border: "2px solid #ba6b6c",
     textAlign: "center",
     height: "10px",
-   width: "300px",
+    width: "300px",
   },
 }));
 
 const Counter = styled("span")(({ theme }) => ({
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-    boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
-    border: "0.5px solid  #fff8e1",
-    background:"#fff8e1",
-    borderRadius: "5px",
-    height: "80%",
-  }));
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+  justifyContent: "space-between",
+  boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+  border: "2px dashed  #ba6b6c",
+  borderRadius: "5px",
+  height: "80%",
+}));
 
-  const Titles = styled("h3")(({ theme }) => ({
-    boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
-    padding: 10,
-    margin: 5,
-    color: "black",
-    backgroundColor: "#fff8e1",
-    borderRadius: 5,
-  }));
+const Titles = styled("h3")(({ theme }) => ({
+  boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+  padding: 10,
+  margin: 5,
+  color: "black",
+  backgroundColor: "#fff8e1",
+  borderRadius: 5,
+}));
 const TableBasket = (props) => {
   const { info, index, updateData, handleCloseModal } = props;
   const [counter, setCounter] = useState(info?.orderCount);
@@ -72,125 +70,128 @@ const TableBasket = (props) => {
   const [isValidIncrease, setIsValidIncrease] = useState(true);
   const [isValidDicrease, setIsValidDicrease] = useState(true);
   const [notValid, setNotValid] = useState(false);
-  const [openDelete,setOpenDelete] = useState(false)
+  const [openDelete, setOpenDelete] = useState(false);
   const dispatch = useDispatch();
   const products = useSelector((state) => state);
-//**modal **//
-const [open, setOpen] = useState(false);
-const [bodyMassages, setBodyMassages] = useState("");
-const [classname, setClassname] = useState("");
+  //**modal **//
+  const [open, setOpen] = useState(false);
+  const [bodyMassages, setBodyMassages] = useState("");
+  const [classname, setClassname] = useState("");
 
-//--------Modal open & close :----------
-const handleShow = () => {
-  setOpen(true);
-  setClassname("failer");
-};
-const handleClose = () => setOpen(false);
-//-----dollarUSLocale:---
-let dollarUSLocale = Intl.NumberFormat("en-US");
+  //--------Modal open & close :----------
+  const handleShow = () => {
+    setOpen(true);
+    setClassname("failer");
+  };
+  const handleClose = () => setOpen(false);
+  //-----dollarUSLocale:---
+  let dollarUSLocale = Intl.NumberFormat("en-US");
 
-//-----saveCount:----
-const cancel = () => {
-  //alert("Cancelled");
-};
+  //-----saveCount:----
+  const cancel = () => {
+    //alert("Cancelled");
+  };
 
-const saveData = (input) => {
-  if (input > Number(info?.count)) {
-    setCounter("1");
-    setBodyMassages(
-      `  موجودی این کالا ${info?.count} عدد است  ، تعداد مورد نظر شما از موجودی بیشتر است `
-    );
-    handleShow();
-  } else if (input <= 0) {
-    setCounter("1");
-    setBodyMassages(` عدد بزرگتر از 1 وارد کنید`);
-    handleShow();
-  } else {
-    setCounter(input);
-  }
-};
+  const saveData = (input) => {
+    if (input > Number(info?.count)) {
+      setCounter("1");
+      setBodyMassages(
+        `  موجودی این کالا ${info?.count} عدد است  ، تعداد مورد نظر شما از موجودی بیشتر است `
+      );
+      handleShow();
+    } else if (input <= 0) {
+      setCounter("1");
+      setBodyMassages(` عدد بزرگتر از 1 وارد کنید`);
+      handleShow();
+    } else {
+      setCounter(input);
+    }
+  };
 
-useEffect(() => {
-  if (info?.count == counter) {
-    setIsValidIncrease(false);
-  } else {
-    setIsValidIncrease(true);
-  }
-  if (counter == 1) {
-    setIsValidDicrease(false);
-  } else {
-    setIsValidDicrease(true);
-  }
-  if (info?.count == 0) {
-    setCounter(0);
-    setNotValid(true);
-    setIsValidDicrease(false);
-    setIsValidIncrease(false);
-    setIsValidShopping(false);
-  }
-}, [counter]);
-//------------
-const handleIncrease = () => {
-  if (info.count !== 0 && info.count > +counter) {
-    setCounter(+counter + 1);
-  }
-};
-//------------
-const handleDicrease = () => {
-  if (+counter > 1) {
-    setCounter(+counter - 1);
-  }
-};
+  useEffect(() => {
+    if (info?.count == counter) {
+      setIsValidIncrease(false);
+    } else {
+      setIsValidIncrease(true);
+    }
+    if (counter == 1) {
+      setIsValidDicrease(false);
+    } else {
+      setIsValidDicrease(true);
+    }
+    if (info?.count == 0) {
+      setCounter(0);
+      setNotValid(true);
+      setIsValidDicrease(false);
+      setIsValidIncrease(false);
+      setIsValidShopping(false);
+    }
+  }, [counter]);
+  //------------
+  const handleIncrease = () => {
+    if (info.count !== 0 && info.count > +counter) {
+      setCounter(+counter + 1);
+    }
+  };
+  //------------
+  const handleDicrease = () => {
+    if (+counter > 1) {
+      setCounter(+counter - 1);
+    }
+  };
 
-const handleShopUpdate = (info, counter) => {
-  dispatch(removeSelectedProduct(info));
-};
+  const handleShopUpdate = (info, counter) => {
+    dispatch(removeSelectedProduct(info));
+  };
 
   return (
     <TableContainer component={Paper} sx={{ mx: "auto", mt: 2 }}>
-      <Table sx={{ direction: "ltr" }}>
+      <Table>
         <TableRow key={info?.id}>
           <TableCells align="left">
             <DeleteForeverIcon
-              clickHandler={() => handleShopUpdate(info, counter)} sx={{ mr: 3 }} />
+              clickHandler={() => handleShopUpdate(info, counter)}
+              sx={{ mr: 3 }}
+            />
           </TableCells>
+          <TableCells>
+            <Counter sx={{ weight: "100%" }}>
+              <Button
+                variant="outlined"
+                sx={{ height: "100%", fontSize: 20, p: 0 }}
+                onClick={handleDicrease}
+                disabled={!isValidDicrease}
+              >
+                -
+              </Button>
+              <Box sx={{ p: 3, fontSize: 15, fontFamily: "SansWeb" }}>
+                <EasyEdit
+                  type="number"
+                  onSave={(e) => saveData(e)}
+                  onCancel={cancel}
+                  saveButtonLabel="ذخیره "
+                  cancelButtonLabel="لغو "
+                  attributes={{ name: "awesome-input", id: 1 }}
+                  value={counter}
+                  instructions={`تعداد موجودی :${info?.count}`}
+                />
+              </Box>
+              <Button
+                variant="outlined"
+                sx={{ height: "100%", fontSize: 20, p: 0 }}
+                onClick={handleIncrease}
+                disabled={!isValidIncrease}
+              >
+                +
+              </Button>
+            </Counter>
+          </TableCells>
+          <TableCells>{info?.price}</TableCells>
           <TableCells sx={{ fontSize: 20, fontFamily: "SansWeb" }}>
             {info?.category.name}
           </TableCells>
-          <TableCells >{info?.name}</TableCells>
-          <TableCells >{info?.price}</TableCells>
-          <TableCells>     
-              <Counter >
-                <Button
-                  variant="outlined"
-                  sx={{ height: "100%", fontSize: 20, p: 0 }}
-                  onClick={handleIncrease}
-                  disabled={!isValidIncrease}
-                >
-                  +
-                </Button>
-                <Box sx={{ p: 3, fontSize:15, fontFamily: "SansWeb" }}>
-                  <EasyEdit
-                    type="number"
-                    onSave={(e) => saveData(e)}
-                    onCancel={cancel}
-                    saveButtonLabel="ذخیره "
-                    cancelButtonLabel="لغو "
-                    attributes={{ name: "awesome-input", id: 1 }}
-                    value={counter}
-                    instructions={`تعداد موجودی :${info?.count}`}
-                  />
-                </Box>
-                <Button
-                  variant="outlined"
-                  sx={{ height: "100%", fontSize: 20, p: 0 }}
-                  onClick={handleDicrease}
-                  disabled={!isValidDicrease}
-                >
-                  -
-                </Button>
-              </Counter>
-            </TableCells>
+
+          <TableCells>{info?.name}</TableCells>
           <TableCells>
             <img
               style={{ width: "80px" }}
