1ede576d99552f3ffc27860a0e658e5d798ab480
narges Hajabolhasan <narges.abolhasan@gmail.com>
2022-05-30 19:43:24 +0430

    feat : paymentHTML created at payment directry

---
+++ paymentHTML/main.js
@@ -0,0 +1,10 @@
+function succsessHandler(){
+    //console.log("Function")
+    window.open("http://localhost:3000/");
+}
+
+function faileHandler(){
+    //console.log("Function")
+    window.open("http://localhost:3000/");
+
+}
