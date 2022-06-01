e834f0ec6a279d4a9fffe2afca8a6ab2c62a2209
narges Hajabolhasan <narges.abolhasan@gmail.com>
2022-06-01 12:48:01 +0430

    refactor : basketRedux directory removed

--- front/src/components/user/ConfigPayment.js
+++ front/src/components/user/ConfigPayment.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {updateProducts,increase,decrease,removeAll } from "../../redux/basketSlice";
+import HttpService from "../../axios/HttpService"
+
 
 const ConfigPayment = () => {
+    const dispatch = useDispatch();
+    const products = useSelector((state) => state);
+
+    useEffect(() => {
+        HttpService.post
+    },[])
+
+    // await HttpService.post("/products", formData);
+    // props.updateData();
+    // props.handleClose();
   return (
     <div>ConfigPayment</div>
   )
--- front/src/redux/basketRedux/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import { productsReducer, selectedProductsReducer } from "./productReducer";
-const reducers = combineReducers({
-  allProducts: productsReducer,
-  selectedProduct: selectedProductsReducer,
-});
-export default reducers;
