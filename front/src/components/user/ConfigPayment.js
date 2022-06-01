7c63e4a9dc8a8427eb0d372331c6089428f3ff49
narges Hajabolhasan <narges.abolhasan@gmail.com>
2022-06-01 14:40:55 +0430

    feat : send informations of customer to backend added

--- front/src/components/user/ConfigPayment.js
+++ front/src/components/user/ConfigPayment.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState,memo } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {setcustomer } from "../../redux/customerSlice";
 import HttpService from "../../axios/HttpService"
@@ -7,16 +7,27 @@ import HttpService from "../../axios/HttpService"
 const ConfigPayment = () => {
     const dispatch = useDispatch();
     const store = useSelector((state) => state);
-    // useEffect(() => {
-    //     dispatch(setcustomer())
-    // },[])
-console.log(store.customer)
-    // await HttpService.post("/products", formData);
-    // props.updateData();
-    // props.handleClose();
+    const data= {
+      "customerDetail":
+        store.customer,
+      "orderStatusId": 3,
+      "deliveredAt":"",
+      "orderItems": store.products,
+    };
+
+  //-----------
+  // useEffect(() => {
+  //   postData();
+  // }, []);
+  // //-----------
+  const postData = useCallback(async () => {
+    await HttpService.post("/exam",data);
+  })()
+
+
   return (
-    <div>ConfigPayment</div>
+    <div style={{fontSize:20}}> با تشکر</div>
   )
 }
 
-export default ConfigPayment
+export default memo(ConfigPayment)
