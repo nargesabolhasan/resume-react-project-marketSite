9965cf3d21a80ade8eb5b7eebabe1ece2428b1f8
narges Hajabolhasan <narges.abolhasan@gmail.com>
2022-06-07 18:46:53 +0430

    fix :  window.location.href added at payment page to navigate resultPayment

--- paymentHTML/main.js
+++ paymentHTML/main.js
@@ -1,14 +1,11 @@
 function succsessHandler(){
-    //console.log("Function")
-    //console.log("")window.open("http://localhost:3000/Payment");
 
-    //return view('http://localhost:3000/Payment');
-    window.open("http://localhost:3000/ResultPayment?success");
+    //window.open("http://localhost:3000/ResultPayment?success");
+    window.location.href = "http://localhost:3000/ResultPayment?success";
 }
 
 function faileHandler(){
-    //console.log("Function")
-    //window.open("http://localhost:3000/Payment");
-    window.open('http://localhost:3000/ResultPayment?failer');
-  //window.open("http://localhost:3000/Payment");
+   // window.open('http://localhost:3000/ResultPayment?failer');
+    window.location.href = 'http://localhost:3000/ResultPayment?failer';
+ 
 }
