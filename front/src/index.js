bb8cf2f8cb1a06b91eaa25be6df345ae08eb1d49
narges Hajabolhasan <narges.abolhasan@gmail.com>
2022-06-07 15:00:50 +0430

    feat :React.StrictMode removed from index.js to sends requset once time

--- front/src/index.js
+++ front/src/index.js
@@ -9,7 +9,7 @@ import StoreProvider from "./redux/provider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <BrowserRouter>
         <StoreProvider>
           <Theme>
@@ -17,7 +17,7 @@ root.render(
           </Theme>
         </StoreProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
