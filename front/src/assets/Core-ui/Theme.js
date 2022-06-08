18e30e7d160ab152a2ba28485fa9d2e712952915
narges Hajabolhasan <narges.abolhasan@gmail.com>
2022-06-08 16:27:55 +0430

    style: theme.palette.Shadow.main added to palette

--- front/src/assets/Core-ui/Theme.js
+++ front/src/assets/Core-ui/Theme.js
@@ -16,21 +16,24 @@ const theme = createTheme({
     SLight: {
       main: "#ef9a9a",
     },
+    Shadow: {
+      main: "#ba6b6c37",
+    },
     warning: {
       main: "#ffc400",
     },
-    amber:{
-      main :"#fff8e1"
+    amber: {
+      main: "#fff8e1",
     },
     success: {
-      main :"rgb(68, 178, 110)"
+      main: "rgb(68, 178, 110)",
     },
-    successGradient:{
-      main :"linear-gradient(90deg, rgba(51,131,130,1) 0%, rgba(148,255,85,1) 40%, rgba(151,255,90,1) 72%, rgba(255,255,255,1) 100%)"
+    successGradient: {
+      main: "linear-gradient(90deg, rgba(51,131,130,1) 0%, rgba(148,255,85,1) 40%, rgba(151,255,90,1) 72%, rgba(255,255,255,1) 100%)",
     },
-    fail:{
-      main :"#d1484a"
-    }
+    fail: {
+      main: "#d1484a",
+    },
   },
   direction: "rtl",
 });
--- front/src/components/user/HomeCategoty.js
+++ front/src/components/user/HomeCategoty.js
@@ -3,8 +3,17 @@ import useGetAxios from "../../axios/useGetAxios";
 import { useNavigate, Outlet } from "react-router-dom";
 import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+import macmini from "../../assets/images/HomePage/1.webp";
+import mac16 from "../../assets/images/HomePage/2.webp";
+import mac14 from "../../assets/images/HomePage/3.webp";
+import mac13 from "../../assets/images/HomePage/4.webp";
+import macAir from "../../assets/images/HomePage/5.webp";
+import imac from "../../assets/images/HomePage/6.webp";
+import ipadPro from "../../assets/images/HomePage/7.webp";
+
 const Titles = styled(Typography)(({ theme }) => ({
   [theme.breakpoints.down("md")]: {
     fontSize: "20px",
@@ -20,9 +29,22 @@ const Titles = styled(Typography)(({ theme }) => ({
   },
 }));
 
+const CardWraper = styled(Paper)(({ theme }) => ({
+  [theme.breakpoints.down("md")]: {
+
+  },
+  [theme.breakpoints.up("md")]: {
+
+  },
+  [theme.breakpoints.up("lg")]: {
+     backgroundColor:theme.palette.Shadow.main
+  },
+}));
+
 const HomeCategoty = () => {
   let navigate = useNavigate();
   const { data, loading, error } = useGetAxios(`/categories/?_embed=products`);
+  const images=[macmini,mac16,mac14,mac13,macAir,imac,ipadPro]
   const handleNavigate = (id) => {
     navigate(`/categories/${id}`);
   };
@@ -32,12 +54,12 @@ const HomeCategoty = () => {
       {loading ? (
         <h1>Loading...</h1>
       ) : (
-        <Box item xs={6}>
-          {data?.data.map((item) => (
-            <Grid item xs={6} key={item.id} onClick={handleNavigate}>
-              
+        <Box item xs={12}>
+          {data?.data.map((item,index) => (
+            <CardWraper item xs={6} key={item.id} onClick={handleNavigate}>
+              <img src={images[index]} alt ="test" style={{width:300}}/>
               <Titles>{item.name}</Titles>
-            </Grid>
+            </CardWraper>
           ))}
         </Box>
       )}
